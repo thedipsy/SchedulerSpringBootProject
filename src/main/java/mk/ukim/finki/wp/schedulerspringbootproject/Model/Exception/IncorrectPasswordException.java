cbc5039c6135804397c0f8ae0b1bd4d98acba467
Angela Milosheska <milosheskaa@gmail.com>
2022-05-11 01:03:29 +0200

    Removed wrong response status in the exceptions.

--- src/main/java/mk/ukim/finki/wp/schedulerspringbootproject/Model/Exception/IncorrectPasswordException.java
+++ src/main/java/mk/ukim/finki/wp/schedulerspringbootproject/Model/Exception/IncorrectPasswordException.java
@@ -3,7 +3,6 @@ package mk.ukim.finki.wp.schedulerspringbootproject.Model.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class IncorrectPasswordException extends RuntimeException {
 
     public IncorrectPasswordException() {
